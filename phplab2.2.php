71033380719c69332b235d6a237befd377019ddb
Shehory <sm.shehory7@gmail.com>
2025-12-10 14:24:42 +0600

    php labtask

---
+++ phplab2.2.php
@@ -0,0 +1,8 @@
+<?php
+
+$amount = 200;
+$vatrate = 0.05;
+$vat = $amount * $vatrate;
+echo "the vat amount is:" . $vat;
+
+?>
---
+++ phplab2.php
@@ -0,0 +1,9 @@
+<?php
+$length = 10;
+$wedth = 5;
+$area = $length * $wedth;
+echo "The area is " . $area;
+echo "\n";
+$perimeter = 2 * ($length + $wedth);
+echo "The perimeter of the rectangle is: " . $perimeter;
+?>
